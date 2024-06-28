de0b3493df251dfe19d4aa8d2e442d2a383ba900
Bread806 <bsw0210903@gmail.com>
2024-06-29 04:01:16 +0800

    test on wan

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
         e.preventDefault();  // 阻止表單默認提交
         const text = document.getElementById('barcodeInput').value.trim();  // 獲取用戶輸入並去除首尾空格
         
-        if (!text) {
-            alert("Please enter some text.");  // 輸入為空時彈出提示
-            return;
-        }
+        // if (!text) {
+        //     alert("Please enter some text.");  // 輸入為空時彈出提示
+        //     return;
+        // }
 
-        alert("You entered: " + text);  // 彈出對話框顯示輸入資料
+        // alert("You entered: " + text);  // 彈出對話框顯示輸入資料
 
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
         const image = new Image();
-        image.src = 'Itachi.jpg';  // 這裡是你圖片的路徑
+        image.src = 'wan_resize.png';  // 這裡是你圖片的路徑
 
         image.onload = function() {
             // 清空 canvas
@@ -25,9 +25,9 @@ document.addEventListener('DOMContentLoaded', function() {
             const barcodeCanvas = document.createElement('canvas');
             JsBarcode(barcodeCanvas, text, {
                 format: "CODE39",
-                lineColor: "#0aa",
+                lineColor: "#402B3A",
                 width: 2,
-                height: 100,
+                height: 80,
                 displayValue: false,
                 background : "rgba(255,255,255,0)"
 
@@ -37,8 +37,10 @@ document.addEventListener('DOMContentLoaded', function() {
             const barcodeImage = new Image();
             barcodeImage.src = barcodeCanvas.toDataURL();
             barcodeImage.onload = function() {
-                const barcodeX = canvas.width - barcodeCanvas.width - 10;  // 條碼 X 坐標
-                const barcodeY = canvas.height - barcodeCanvas.height - 10; // 條碼 Y 坐標
+                // const barcodeX = canvas.width - barcodeCanvas.width - 10;  // 條碼 X 坐標
+                // const barcodeY = canvas.height - barcodeCanvas.height - 10; // 條碼 Y 坐標
+                const barcodeX = canvas.width / 10 + 25;  // 條碼 X 坐標
+                const barcodeY = canvas.height *2/3 ; // 條碼 Y 坐標
                 ctx.drawImage(barcodeImage, barcodeX, barcodeY);
             };
         };
